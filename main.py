5055288e68f1ffa3d771bbebf2c0e2baa9e8e0cd
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2022-05-07 06:16:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ location = ['USA', 'Dhaka', 'Khulna']
 i = 0 #list Index and Loop
 dicts_list = [] #Dict List
 while i < len(company_name):
-   dicts = {}
+   dicts = {} #Write New Dict For Each While_Loop
    dicts['company_name'] = company_name[i]
    dicts['postion'] = postion[i]
    dicts['location'] = location[i]
-   dicts_list.append(dicts)
+   dicts_list.append(dicts) #Adding New List For Each While_Loop With Existing Data
 
    print(i)
    print(dicts)
