cc068ad8a48067b3cccf57ba234a0b86c6d7ebb9
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-08-02 23:15:04 +0600

    Update list-writer.py

--- list-writer.py
+++ list-writer.py
@@ -1,6 +1,5 @@
 import csv
 
-
 # field names
 fields = ['Name', 'Branch', 'Year', 'CGPA']
 	
@@ -10,12 +9,29 @@ rows = [ ['Nikhil', 'COE', '2', '9.0'],
 		['Aditya', 'IT', '2', '9.3'],
 		['Sagar', 'SE', '1', '9.5'],
 		['Prateek', 'MCE', '3', '7.8'],
-		['Sahil', 'EP', '2', '9.1']]
+		['Sahil', 'EP', '2', '9.1']
+       ]
 
 with open('test.csv', 'w') as file:
-	
 	# using csv.writer method from CSV package
 	write = csv.writer(file)
 	
 	write.writerow(fields)
 	write.writerows(rows)
+
+
+# Data Writing example
+import csv
+datas = [
+	['Samrat', '003', 'Boikali'],
+	['Fahim', '013', 'Doulatpur'],
+]
+for single_data in datas:
+	write_list = [single_data[0], single_data[1], single_data[2]]
+	write_list.append(write_list)
+	header = ['Name','ID','Address']
+	with open('test.csv', 'w') as file:
+		write = csv.writer(file)
+		write.writerow(header)
+		write.writerows(write_list)
+	
